0498b33ed17638f9c1687f7ed369c633c9326ac0
Denperidge <denperidge@gmail.com>
2023-10-01 13:28:24 +0200

    Added scss

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,21 @@
 const yaml = require("js-yaml");
-const glob = require("glob");
-
-
-function getProjects() {
-    console.log(glob.GlobSync("src/projects/*.md"));
-}
+const sass = require("sass");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
-    eleventyConfig.addFilter("getProjects", getProjects)
+
+    
+    eleventyConfig.addTemplateFormats("scss")
+    eleventyConfig.addExtension("scss", {
+        outputFileExtension: "css",
+        compile: async function(input) {
+            let result = sass.compileString(input);
+
+            return async (data) => {
+                return result.css;
+            }
+        }
+    })
     
     return {
         dir: { 
