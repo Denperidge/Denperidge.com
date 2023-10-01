f8cbad9f7d66b2b48c056e10277eeebb09680b10
Denperidge <denperidge@gmail.com>
2023-10-01 12:32:32 +0200

    WIP Collections based nav

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,15 @@
+const yaml = require("js-yaml");
+const glob = require("glob");
+
+
+function getProjects() {
+    console.log(glob.GlobSync("src/projects/*.md"));
+}
+
 module.exports = function(eleventyConfig) {
+    eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
+    eleventyConfig.addFilter("getProjects", getProjects)
+    
     return {
         dir: { 
             input: "src",
---
+++ src/projects/nimona-archive.md
@@ -0,0 +1,7 @@
+---
+layout: project.pug
+title: "The Nimona Archive"
+---
+# The Nimona Archive
+Meow
+
