44404fff5da14a809e04af3d88170cfd123d748b
Denperidge <denperidge@gmail.com>
2023-11-08 16:36:00 +0100

    Removed old assets, added Dendy.jpg favicon

--- .favicons.js
+++ .favicons.js
@@ -11,9 +11,9 @@ async function write(response, property, outputDir) {
 }
 
 module.exports = async function(eleventyDir) {
-    const response = await favicons("src/assets/favicon.svg", {
-        background: "white",
-        theme_color: "white",
+    const response = await favicons("src/assets/Dendy.jpg", {
+        background: "#f4f6a3",
+        theme_color: "#f4f6a3",
     });
     await fs.writeFile(path.join("src", "_includes", "base", "favicons.pug"), response.html.join(""), ()=>{})
     await write(response, "images", eleventyDir.output);
