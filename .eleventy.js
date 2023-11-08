c9ab2a84782f7965b2fbec18a9b5f15f64b74f79
Denperidge <denperidge@gmail.com>
2023-11-08 16:32:07 +0100

    Futher reformats

--- .eleventy.js
+++ .eleventy.js
@@ -12,6 +12,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
 
     eleventyConfig.addPassthroughCopy("src/assets");
+    eleventyConfig.addPassthroughCopy("src/scripts");
     eleventyConfig.addPlugin(eleventySass, {
         sass: {
             loadPaths: ["node_modules"]
