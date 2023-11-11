3040c4b294ff50c975c9b4d299f364b922f6ef38
Denperidge <denperidge@gmail.com>
2023-11-11 13:02:58 +0100

    Created dynamic hover & focus system

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,51 +1,78 @@
 const spans = document.querySelectorAll("#denperidge h1 span");
+const explanations = document.querySelectorAll("#denperidge section");
 
-tooltipData = {
-    "Dendy": {
-        colour: "aqua",
-        text: "Dendy, from OK K.O.!"
-    },
-    "entrapta": {
-        colour: "purple",
-        text: "Entrapta, from She-Ra!",
-    },
-    "peridot": {
-        colour: "green",
-        text: "Peridot, from Steven Universe!"
-    },
-    "pidge": {
-        colour: "lime",
-        text: "Pidge, from Voltron!"
+function onEverySpan(func) {
+    for (let i = 0; i < spans.length; i++) {
+        func(spans[i]);
     }
 }
 
+function hideEveryExplanation() {
+    for (let i = 0; i < explanations.length; i++) {
+        explanations[i].style.display = "none";
+        explanations[i].ariaDisabled = "true";
+    }
+}
 
-function onLetterEnter(selector, key) {
-    document.getElementById(selector).addEventListener("mouseover", function(){
-        for (let i = 0; i < spans.length; i++) {
-            const span = spans[i];
-            if (key.indexOf(span.id) > -1) {
-                span.style.color = tooltipData[key].colour;
-            }
+function addLetterColours(e, character) {
+    console.log(e)
+
+    onEverySpan((span)=> {
+        if (character.colourtext.indexOf(span.id) > -1) {
+            span.style.color = character.colour;
         }
     });
-}
 
-function showTooltip(selector) {
-    document.addEventListener("mousemove", function(e){
-        console.log(e)
+    hideEveryExplanation();
+
+    explanation = document.getElementById(character.name);
+    explanation.ariaDisabled = false;
+    explanation.style.display = "initial";
+    explanation.style.position = "absolute";
+
+
+    let x, y;
+
+    if (e.type == "mousemove") {
+        x = e.layerX;
+        y = e.layerY + 25;
+    } else {
+        const pos = e.target.getBoundingClientRect();
+        console.log(pos)
+        x = pos.left + 50;
+        y= pos.top - 350;
+    }
+    explanation.style.left = x + "px";
+    explanation.style.top = y + "px";
+};
+
+function removeLetterColours() {
+    onEverySpan((span) => {
+        span.style.color = "initial";
     });
+    hideEveryExplanation();
 }
 
-function tooltip(selector, key) {
-    onLetterEnter(selector, key)
-    showTooltip(selector)
+function setupEvents(selector, character) {
+    document.getElementById(selector).addEventListener("mousemove", (e) => { addLetterColours(e, character); });
+    document.getElementById(selector).addEventListener("focus", (e) => { addLetterColours(e, character); });
+    document.getElementById(selector).addEventListener("focusout", removeLetterColours);
+    document.addEventListener("mouseover", (e) => {
+        if (e.target.parentNode.tagName != "H1") {
+            removeLetterColours();
+        }
+    });
+
 }
 
-onLetterEnter("D", "Dendy");
-onLetterEnter("en", "entrapta");
-onLetterEnter("per", "peridot");
-onLetterEnter("i", "peridot");
-onLetterEnter("d", "pidge");
-onLetterEnter("ge", "pidge");
+// Hide the title (aside from screenreaders) and other explanations if js is enabled
+document.querySelector("#explanation h2").classList.add("sr-only");
+hideEveryExplanation();
+
+setupEvents("D", characters[0]);
+setupEvents("en", characters[1]);
+setupEvents("per", characters[2]);
+setupEvents("i", characters[2]);
+setupEvents("d", characters[3]);
+setupEvents("ge", characters[3]);
 
